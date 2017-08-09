3cf3fe668d67bcd19ed9c3abaff8b073011ff89f
mirzasb <mirzasbaig@nyu.edu>
2017-08-09 10:40:34 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+app.use('/maintenance', (req, res, next) => {
+    res.render('maintenance.hbs', {
+        errorMessage: 'The page is being updated. Please try again later.'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+   return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+        //res.send('<h1>Hello Express</h1>');
+        // res.send({
+        // name: 'Andrew',
+        // likes: [
+        //     'Biking',
+        //     'Cities'
+        // ]
+        res.render('home.hbs', {
+            pageTitle: 'Homepage',
+            welcomeTxt: 'Welcome to the Homepage'
+        });
+    }
+);
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle the request'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
